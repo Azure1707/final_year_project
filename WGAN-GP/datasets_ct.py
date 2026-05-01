2e8c65bd4f96b77c690ba5fa4de366e7603c01e7
Riwaz Gurung <riwazgurung17@yahoo.com>
2026-05-01 11:16:33 +0100

    slight clean ups

--- WGAN-GP/datasets_ct.py
+++ WGAN-GP/datasets_ct.py
@@ -4,10 +4,9 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 
-# ===== import your preprocessing =====
 from preprocess_roi import preprocess_roi_from_uint16png
 
-
+#Dataset class for loading data to model
 class CTImageDataset(Dataset):
     def __init__(self, root_dir, image_size=128):
         self.image_paths = []
@@ -29,10 +28,10 @@ class CTImageDataset(Dataset):
     def __getitem__(self, idx):
         path = self.image_paths[idx]
 
-        # load as uint16 (VERY IMPORTANT)
+        # load as uint16
         img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
 
-        # apply your preprocessing
+        # apply preprocessing
         img = preprocess_roi_from_uint16png(img, size=self.image_size)
 
         # convert to tensor
